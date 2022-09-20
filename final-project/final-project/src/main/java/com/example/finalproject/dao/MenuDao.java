9116be586a738e5d1d50e79ae925c6af17cdae1a
Nishanth-AB <47138975+Nishanth-AB@users.noreply.github.com>
2022-09-20 17:28:18 +0530

    Update MenuDao.java

--- final-project/final-project/src/main/java/com/example/finalproject/dao/MenuDao.java
+++ final-project/final-project/src/main/java/com/example/finalproject/dao/MenuDao.java
@@ -1,28 +1,37 @@
-package com.example.finalproject.dao;
+package com.cl.foodapp.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.example.finalproject.dto.Menu;
-
-import com.example.finalproject.repository.MenuRepository;
-
+import com.cl.foodapp.entity.Menu;
+import com.cl.foodapp.repository.MenuRepository;
 
 @Repository
 public class MenuDao {
-
-	@Autowired
-	MenuRepository menuRepository;
 	
+	@Autowired
+	private MenuRepository menuRepository;
 	public Menu saveMenu(Menu menu) {
 		return menuRepository.save(menu);
 	}
-	public Menu updateMenu(Menu menu) {
+	public Optional<Menu> getby(int id) {
+		return menuRepository.findById(id);
+	}
+	public Menu delete(int id) {
+		Menu menu=getby(id).get();
+		menuRepository.delete(menu);
+		return menu;
+	}
+	public Menu update(Menu menu, int id) {
+		menu.setId(id);
 		return menuRepository.save(menu);
 	}
-	
+	public List<Menu> getall(){
+		return menuRepository.findAll();
+	}
 	public Menu getMenuById(int id) {
 		Optional <Menu>found=menuRepository.findById(id);
 		if(found.isPresent()) {
@@ -31,5 +40,4 @@ public class MenuDao {
 			return null;
 		}
 	}
-	
 }
