5e80d1770e01c2e06ef5e019a0255985394b84d9
kushalhu123 <55405047+kushalhu123@users.noreply.github.com>
2022-09-20 17:28:32 +0530

    Update FoodProductService.java

--- final-project/final-project/src/main/java/com/example/finalproject/service/FoodProductService.java
+++ final-project/final-project/src/main/java/com/example/finalproject/service/FoodProductService.java
@@ -1,41 +1,82 @@
-package com.example.finalproject.service;
+package com.cl.foodapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.example.finalproject.dao.FoodProductDao;
-import com.example.finalproject.dao.MenuDao;
-import com.example.finalproject.dto.FoodProduct;
-import com.example.finalproject.dto.Menu;
-import com.example.finalproject.dto.User;
+
+import com.cl.foodapp.Exception.ProductNotFound;
+import com.cl.foodapp.dao.MenuDao;
+import com.cl.foodapp.dao.ProductDao;
+import com.cl.foodapp.entity.Menu;
+import com.cl.foodapp.entity.Products;
+import com.cl.foodapp.responseStructure.ResponseStructure;
 
 @Service
-public class FoodProductService {
+public class ProductsService {
 
 	@Autowired
-	FoodProductDao foodProductDao;
+	ProductDao productDao;
+	
 	@Autowired
 	MenuDao menuDao;
 	
-	public FoodProduct saveFoodProduct(FoodProduct foodProduct, int id) {
-		Menu menu=menuDao.getMenuById(id);
-		foodProduct.setMenu(menu);
-		return foodProductDao.saveFoodProduct(foodProduct);
+	public ResponseEntity<ResponseStructure<Products>> saveProducts(Products products) {
+		ResponseStructure<Products> responseStructure = new ResponseStructure<>();
+		responseStructure.setMessage("Saved Successfully");
+		responseStructure.setStatuscode(HttpStatus.CREATED.value());
+		responseStructure.setT(productDao.saveProducts(products));
+		return new ResponseEntity<ResponseStructure<Products>>(responseStructure, HttpStatus.CREATED);
 	}
-	public FoodProduct updateFoodProduct(FoodProduct foodProduct, int id) {
-		Menu menu=menuDao.getMenuById(id);
-		foodProduct.setMenu(menu);
-		return foodProductDao.updateFoodProduct(foodProduct);
+	public ResponseEntity<ResponseStructure<Products>> getby(int id) {
+		Optional<Products> products = productDao.getby(id);
+		ResponseStructure<Products> responseStructure = new ResponseStructure<>();
+		if (products.isEmpty()) {
+			throw new ProductNotFound();
+		} else {
+			responseStructure.setMessage("Found Successfully");
+			responseStructure.setStatuscode(HttpStatus.OK.value());
+			responseStructure.setT(products.get());
+			return new ResponseEntity<ResponseStructure<Products>>(responseStructure, HttpStatus.OK);
+		}
+
 	}
-	public List<FoodProduct> getAllFoodProducts(){
-		return foodProductDao.getAllFoodProduct();
+	public ResponseEntity<ResponseStructure<Products>> delete(int id) {
+		Optional<Products> products = productDao.getby(id);
+		ResponseStructure<Products> responseStructure = new ResponseStructure<>();
+		if (products.isEmpty()) {
+			throw new ProductNotFound();
+		} else {
+			responseStructure.setMessage("deleted Successfully");
+			responseStructure.setStatuscode(HttpStatus.OK.value());
+			responseStructure.setT(productDao.delete(id));
+			return new ResponseEntity<ResponseStructure<Products>>(responseStructure, HttpStatus.OK);
+		}
+
 	}
-	public FoodProduct getFoodProductById(int id) {
-		return foodProductDao.getFoodProductById(id);
+	public ResponseEntity<ResponseStructure<Products>> update(Products products, int id) {
+		Optional<Products> products2 = productDao.getby(id);
+		ResponseStructure<Products> responseStructure = new ResponseStructure<>();
+		if (products2.isEmpty()) {
+			throw new ProductNotFound();
+		} else {
+			responseStructure.setMessage("updated Successfully");
+			responseStructure.setStatuscode(HttpStatus.OK.value());
+			responseStructure.setT(productDao.update(products, id));
+			return new ResponseEntity<ResponseStructure<Products>>(responseStructure, HttpStatus.OK);
+		}
+
 	}
-	public String deleteFoodProduct(int id) {
-		return foodProductDao.deleteFoodProduct(id);
+	public ResponseEntity<ResponseStructure<List<Products>>> getall() {
+		ResponseStructure<List<Products>> responseStructure = new ResponseStructure<List<Products>>();
+		responseStructure.setMessage("Found all");
+		responseStructure.setStatuscode(HttpStatus.OK.value());
+		responseStructure.setT(productDao.getall());
+		return new ResponseEntity<ResponseStructure<List<Products>>>(responseStructure, HttpStatus.CREATED);
 	}
 }
+
