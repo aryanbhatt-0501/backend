0efb4c1cf708037408d563ff7856c594599d4e4e
kushalhu123 <55405047+kushalhu123@users.noreply.github.com>
2022-09-20 17:27:41 +0530

    Update MenuService.java

--- final-project/final-project/src/main/java/com/example/finalproject/service/MenuService.java
+++ final-project/final-project/src/main/java/com/example/finalproject/service/MenuService.java
@@ -1,27 +1,80 @@
-package com.example.finalproject.service;
+package com.cl.foodapp.service;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.example.finalproject.dao.MenuDao;
-import com.example.finalproject.dao.UserDao;
-import com.example.finalproject.dto.Menu;
-import com.example.finalproject.dto.User;
+import com.cl.foodapp.dao.MenuDao;
+import com.cl.foodapp.dao.UserDao;
+import com.cl.foodapp.entity.Menu;
+import com.cl.foodapp.entity.User;
+import com.cl.foodapp.responseStructure.ResponseStructure;
 
 @Service
 public class MenuService {
-
+	
 	@Autowired
-	MenuDao menuDao;
+	private MenuDao menuDao;
 	@Autowired
 	UserDao userDao;
+	public ResponseEntity<ResponseStructure<Menu>> saveMenu(Menu menu){
+		ResponseStructure<Menu> responseStructure=new ResponseStructure<>();
+		User user =menu.getUser();
+		int id=user.getId();
+		User user1=userDao.getUserById(id);
+		menu.setUser(user1);
+		responseStructure.setMessage("Saved");
+		responseStructure.setStatuscode(HttpStatus.CREATED.value());
+		responseStructure.setT(menuDao.saveMenu(menu));
+
+		return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.CREATED);}
 	
-	public Menu saveMenu(Menu menu, int id) {
-		User user=userDao.getUserById(id);
-		menu.setUser(user);
-		return menuDao.saveMenu(menu);
-	}
-	public Menu updateMenu(Menu menu) {
-		return menuDao.updateMenu(menu);
-	}
-}
+
+	
+	public ResponseEntity<ResponseStructure<Menu>> getby(int id){
+		Optional<Menu> menu=menuDao.getby(id);
+		ResponseStructure<Menu> responseStructure=new ResponseStructure<>();
+		if(menu.isEmpty()) {
+			responseStructure.setMessage("Id not found");
+			responseStructure.setStatuscode(HttpStatus.NOT_FOUND.value());
+			return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.NOT_FOUND);}
+		else {
+			responseStructure.setMessage("found");
+			responseStructure.setStatuscode(HttpStatus.OK.value());
+			responseStructure.setT(menu.get());
+			return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.OK);}}
+	
+	public ResponseEntity<ResponseStructure<Menu>> delete(int id){
+		Optional<Menu> menu=menuDao.getby(id);
+		ResponseStructure<Menu> responseStructure=new ResponseStructure<>();
+		if(menu.isEmpty()) {
+			responseStructure.setMessage("Id not found");
+			responseStructure.setStatuscode(HttpStatus.NOT_FOUND.value());
+			return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.NOT_FOUND);}
+		else {
+			responseStructure.setMessage("Deleted");
+			responseStructure.setStatuscode(HttpStatus.OK.value());
+			responseStructure.setT(menuDao.delete(id));
+			return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.OK);}}
+	public ResponseEntity<ResponseStructure<Menu>> update(Menu menu,int id){
+		Optional<Menu> menu2=menuDao.getby(id);
+		ResponseStructure<Menu> responseStructure=new ResponseStructure<>();
+		if(menu2.isEmpty()) {
+			responseStructure.setMessage("Id not found");
+			responseStructure.setStatuscode(HttpStatus.NOT_FOUND.value());
+			return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.NOT_FOUND);}
+		else {
+			responseStructure.setMessage("Updated");
+			responseStructure.setStatuscode(HttpStatus.OK.value());
+			responseStructure.setT(menuDao.update(menu, id));
+			return new ResponseEntity<ResponseStructure<Menu>>(responseStructure, HttpStatus.OK);}}
+	public ResponseEntity<ResponseStructure<List<Menu>>> getall(){
+		ResponseStructure<List<Menu>> responseStructure=new ResponseStructure<List<Menu>>();
+		responseStructure.setMessage("Found");
+		responseStructure.setStatuscode(HttpStatus.OK.value());
+		responseStructure.setT(menuDao.getall());
+		return new ResponseEntity<ResponseStructure<List<Menu>>>(responseStructure, HttpStatus.OK);}}
